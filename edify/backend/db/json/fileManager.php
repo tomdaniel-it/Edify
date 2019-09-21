700c5af1fe662d18dcef0bc921df30ce02225580
Tom Daniel <tomdaniel.it@gmail.com>
2019-09-21 22:04:35 +0200

    Update fileManager

--- edify/backend/db/json/fileManager.php
+++ edify/backend/db/json/fileManager.php
@@ -16,19 +16,33 @@ function removePhpContentPrefix($content) {
   return substr($content, 8);
 }
 
+// PRIVATE
+function checkDbDirectory() {
+  if (!is_dir(join(DIRECTORY_SEPARATOR, array(__DIR__, 'db')))) {
+    mkdir(join(DIRECTORY_SEPARATOR, array(__DIR__, 'db')), 0777, FALSE);
+  }
+}
+
+// Always returns an array
 function readFileContent($filename) {
   $path = getFilePath($filename);
   if (!file_exists($path)) {
-    return NULL;
+    return [];
   }
   $jsonLine = removePhpContentPrefix(trim(fgets(fopen($path, 'r'))));
   return json_decode($jsonLine);
 }
 
-function writeFileContent($filename, $obj) {
+function writeFileContent($filename, $arr) {
+  if (!is_array($arr)) {
+    throw new Exception('writeFileContent only allows arrays to be written to files.');
+  }
+
+  checkDbDirectory();
+
   $path = getFilePath($filename);
 
-  $json = json_encode($obj);
+  $json = json_encode($arr);
 
   file_put_contents($path, addPhpContentPrefix($json));
 }
