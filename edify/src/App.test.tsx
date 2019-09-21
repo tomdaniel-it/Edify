967f9e7618b1f29219c80908c9c4d579b840e97e
Tom Daniel <tomdaniel.it@gmail.com>
2019-09-21 17:49:03 +0200

    Test commit with bug

--- edify/src/App.test.tsx
+++ edify/src/App.test.tsx
@@ -4,6 +4,7 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  throw 1;
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
