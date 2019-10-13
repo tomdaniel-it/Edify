ed9c6dc3636ab6426dae7ea6bfcababe58f4854f
Tom Daniel <tomdaniel.it@gmail.com>
2019-10-14 00:45:54 +0200

    Add error & warning handler to util

--- edify/src/util.tsx
+++ edify/src/util.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
 export const isProduction
   = () => !process || !process.env || process.env.NODE_ENV !== 'development';
 
 export const isDevelopment = () => !isProduction();
 
-// eslint-disable-next-line no-alert
-export const handleUnexpectedError = (error: string) => alert(error);
+export const handleError
+  = (error: Error, ...otherInfo: Array<any>) => console.error(error, ...otherInfo);
+
+export const handleWarning
+  = (warning: Error, ...otherInfo: Array<any>) => console.warn(warning, ...otherInfo);
